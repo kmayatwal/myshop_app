2169adcc158ff78deba75468f2de3c2ae75a0a80
Kartikaiya Mayatwal <kartike.mayatwal@artelir.com>
2024-05-21 20:06:36 +0530

    Using Consumer over Provider.of

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -18,57 +18,59 @@ class ProductItem extends StatelessWidget {
     // final productsData = Provider.of<Products>(context, listen: false);
     // Provider.of<Products>(context).addFavorite()
 
-    final product = Provider.of<Product>(context);
+    // final product = Provider.of<Product>(context);
 
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: Container(
-          // decoration: BoxDecoration(
-          //   color: Colors.lime,
-          //   border: Border.all(width: 1, color: Colors.black87),
-          //   borderRadius: BorderRadius.circular(10),
-          // ),
-          child: GestureDetector(
-            onTap: () {
-              // On The Fly Route
-              // Navigator.of(context).push(
-              //   MaterialPageRoute(
-              //     builder: (ctx) => ProductDetailScreen(title),
-              //   ),
-              // );
+    return Consumer<Product>(
+      builder: (ctx, product, child) => ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          child: Container(
+            // decoration: BoxDecoration(
+            //   color: Colors.lime,
+            //   border: Border.all(width: 1, color: Colors.black87),
+            //   borderRadius: BorderRadius.circular(10),
+            // ),
+            child: GestureDetector(
+              onTap: () {
+                // On The Fly Route
+                // Navigator.of(context).push(
+                //   MaterialPageRoute(
+                //     builder: (ctx) => ProductDetailScreen(title),
+                //   ),
+                // );
 
-              Navigator.of(context).pushNamed(
-                ProductDetailScreen.routeName,
-                arguments: product.id,
-              );
-            },
-            child: Image.network(
-              product.imageUrl,
-              fit: BoxFit.cover,
+                Navigator.of(context).pushNamed(
+                  ProductDetailScreen.routeName,
+                  arguments: product.id,
+                );
+              },
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite
-                  ? Icons.favorite
-                  : Icons.favorite_border_outlined,
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(
+              icon: Icon(
+                product.isFavorite
+                    ? Icons.favorite
+                    : Icons.favorite_border_outlined,
+              ),
+              onPressed: () {
+                product.toggleFavoriteStatus();
+                // Provider.of<Products>(context, listen: false).addFavorite(id, product.isFavorite);
+              },
             ),
-            onPressed: () {
-              product.toggleFavoriteStatus();
-              // Provider.of<Products>(context, listen: false).addFavorite(id, product.isFavorite);
-            },
-          ),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {},
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+            ),
           ),
         ),
       ),
